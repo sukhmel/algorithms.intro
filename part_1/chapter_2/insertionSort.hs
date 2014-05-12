048f41b3fdd7d5c1f6aa8dcd172dfc3a627c8fc0
sukhmel <sukhmel.v@gmail.com>
2014-05-13 00:53:52 +0400

    Rewritten insertion sort in a way that it is now really based on insertion, rather than a kind of bubble sort as it was because of being too closely based on imperative pseudo-code of handbook.

--- part_1/chapter_2/insertionSort.hs
+++ part_1/chapter_2/insertionSort.hs
@@ -2,43 +2,50 @@
 
 module InsertSort where
 
-import Test.QuickCheck
 import Data.List (sort)
 
+-- Implementation of insertion sorting. At each step beginning of a list is
+-- already  sorted,  first  element of  ending  is taken  and inserted into
+-- beginning in a way that retains order.
 insertionSortBy :: forall a. (a -> a -> Bool) -> [a] -> [a]
 insertionSortBy f = innerSort 2
-              where
-                    innerSort      :: Int -> [a] -> [a]
-                    innerSort _ [] = []
-                    innerSort n xs =
-                           if n > length xs
-                              then xs
+              where innerSort      :: Int -> [a] -> [a] -- sort n first elements
+                    innerSort _ [] = []                 -- and then sort  n+1
+                    innerSort n xs =                    -- in case n is still
+                           if n > length xs             -- less  or  equal to
+                              then xs                   -- length of the list
                               else innerSort (n+1)
-                                 $ drownSort (take n xs)
-                                           ++ drop n xs
+                                 $ e `insertInto` begin
+                                           ++ rest
+                             where (begin, end) = splitAt (n-1) xs
+                                   e            = head end
+                                   rest         = tail end
 
-                    drownSort             :: [a] -> [a]
-                    drownSort ys@(a:b:cs) =
-                            let begin     = init ys
-                                rest      = init begin
-                                compared  = last ys
-                                candidate = last begin
-                            in  case cs of
-                                     [] -> if b `f` a then [b, a] else [a, b]
-                                     _  -> if compared `f` candidate
-                                             then drownSort (rest ++ [compared])
-                                                                  ++ [candidate]
-                                             else ys
-                    drownSort rest = rest
+                    insertInto          :: a -> [a] -> [a] -- insert an element
+                    e `insertInto` es@(a:as) =             -- into  the list by
+                              let c          = last es     -- given predicate.
+                                  rest       = init es
+                              in  case as of
+                                       [] -> if e `f` a then [e, a] else [a, e]
+                                       _  -> if e `f` c
+                                               then e `insertInto` rest ++ [c]
+                                               else es ++ [e]
+                    e `insertInto` []        = [e]
 
+-- ensure that flipping of predicate creates reversed result (although
+-- that's not really true for predicates same as their flip)
 prop_Negation    :: [Int] -> Bool
-prop_Negation xs = insertionSortBy (<) xs == reverse (insertionSortBy (flip (<)) xs)
+prop_Negation xs = insertionSortBy (flip (<)) xs ==
+          reverse (insertionSortBy (<)        xs)
 
+-- ensure that there is same amount of elements
 prop_Length      :: [Int] -> Bool
 prop_Length   xs = length xs == length (insertionSortBy (<) xs)
 
+-- ensure elements are same after sorting
 prop_Safety      :: [Int] -> Bool
 prop_Safety   xs = sort xs == sort (insertionSortBy (<) xs)
 
+-- a little cheat ;-)
 prop_Correct     :: [Int] -> Bool
 prop_Correct  xs = sort xs == insertionSortBy (<) xs
