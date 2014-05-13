7ea570a9d72f4175b274c6825882439115053c35
sukhmel <sukhmel.v@gmail.com>
2014-05-13 14:27:25 +0400

    Completed task 2.1.3 -- implementation of linear search.

---
+++ part_1/chapter_2/linearSearch.hs
@@ -0,0 +1,32 @@
+-- task 2.1.3
+
+module LinearSearch where
+
+import Data.Maybe(isNothing, fromMaybe)
+import Test.QuickCheck
+
+-- Simple implementation of linear search.
+linSearch      :: Eq a => a -> [a] -> Maybe Int
+linSearch a as = case rs of
+                      []  -> Nothing
+                      v:_ -> Just . snd $ v
+      where rs = dropWhile ((a /=) . fst) zs
+            zs = zip as [0..]
+
+-- Test ensuring that for existing element, position found holds same value
+-- as original.
+prop_Included      :: Int -> [Int] -> Property
+prop_Included a as = a `elem` as    ==>
+                     a == as !! r
+           where r = fromMaybe (-1) $ linSearch a as
+
+-- Test ensuring that for non-existing element search Nothing is returned.
+prop_Excluded      :: Int -> [Int] -> Property
+prop_Excluded a as = a `notElem` as ==>
+                     isNothing $ linSearch a as
+
+-- Test that found value is first possible
+prop_Minimal       :: Int -> [Int] -> Property
+prop_Minimal  a as = a `elem` as    ==>
+                     a `notElem` take (r-1) as
+           where r = fromMaybe (-1) $ linSearch a as
