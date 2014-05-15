4c0b2ee9540bcd0dc1a379f082bb5b4796be20d7
sukhmel <sukhmel.v@gmail.com>
2014-05-16 01:13:30 +0400

    Counting inversions task 2.4 is done. Made use of Writer monad (first time ever, not like I am really sure, things are done right). Still there can be an improvement of actually finding all inversions' indices.

--- part_1/chapter_2/MergeSort.hs
+++ part_1/chapter_2/MergeSort.hs
@@ -1,18 +1,44 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module MergeSort where
 
 import SortingTests
+import Control.Monad.Writer
+
+countInversions ::  MonadWriter [t] m => t -> m ()
+countInversions i = writer ((), [i])
+
+mergeBy :: MonadWriter [Int] m
+        => (a -> a -> Bool)
+        -> [a] -> [a] -> m [a]
+mergeBy _ [] b = return b
+mergeBy _ a [] = return a
+mergeBy f (a:as)
+          (b:bs) = if a `f` b
+                      then do rest <- mergeBy f as (b:bs)
+                              return $ a:rest
+                      else do countInversions $ length as + 1
+                              rest <- mergeBy f (a:as) bs
+                              return $ b:rest
 
 mergeSortBy       :: (a -> a -> Bool) -> [a] -> [a]
-mergeSortBy _ [ ]    = [ ]
-mergeSortBy _ [a]    = [a]
-mergeSortBy f xs     = mergeSortBy f beg
-               `merge` mergeSortBy f end
+mergeSortBy f = fst . runWriter . performMergeSortBy f
+
+-- | Task 2.4 (e) count all invesions in a list pairs (i, j) where i < j, but
+-- a[j] < a[i]
+countInverses :: Ord a => [a] -> Int
+countInverses = sum . snd . runWriter . performMergeSortBy (<)
+
+performMergeSortBy :: MonadWriter [Int] m
+                   => (a -> a -> Bool)
+                   -> [a] -> m [a]
+performMergeSortBy _ [ ]    = return [ ]
+performMergeSortBy _ [a]    = return [a]
+performMergeSortBy f xs     = do
+              first  <- performMergeSortBy f beg
+              second <- performMergeSortBy f end
+              mergeBy f first second
     where (beg, end) = splitAt (length xs `div` 2) xs
-          merge [] b = b
-          merge a [] = a
-          merge (a:as) (b:bs) = if a `f` b
-                                   then a: merge as (b:bs)
-                                   else b: merge (a:as) bs
 
 -- | perform tests with merge sort using quickcheck
 main :: IO ()
