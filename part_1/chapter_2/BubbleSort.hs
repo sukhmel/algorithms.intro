47d8e69288b5c99fe401aaaccc75ae40f83d75e3
sukhmel <sukhmel.v@gmail.com>
2014-05-16 15:58:37 +0400

    Added one more implementation of bubble sort.

--- part_1/chapter_2/BubbleSort.hs
+++ part_1/chapter_2/BubbleSort.hs
@@ -5,6 +5,8 @@ import Control.Monad
 
 import SortingTests
 
+type Predicate a = a -> a -> Bool
+
 -- | Implementation of modified bubble sorting, where reverses let achieve
 -- using of pattern matching and avoid use of (++) in the inner loop.
 bubbleSortBy ::  (a -> a -> Bool) -> [a] -> [a]
@@ -17,6 +19,24 @@ bubbleSortBy f as = case innerSort $ reverse as of
                                   else Just $ b : fromMaybe (a:cs)
                                                 (innerSort $ a:cs)
           innerSort _        = Nothing
+
+-- | Helper function to sort only N first elements of a list (to avoid
+-- splitting and recreating a list like in other implementations)
+bubbleSortNBy :: Int -> Predicate a -> [a] -> [a]
+bubbleSortNBy n f as = case innerSort n as of
+                            Nothing -> as
+                            Just xs -> bubbleSortNBy (n-1) f xs
+    where innerSort 1     _    = Nothing
+          innerSort m (a:b:cs) = if a `f` b
+                                    then liftM (a:) $ innerSort (m-1) (b:cs)
+                                    else Just $ b : fromMaybe (a:cs)
+                                            (innerSort (m-1) $ a:cs)
+          innerSort _     _    = Nothing
+
+-- | Implementation that sorts only first N - i elements on i-th loop.
+-- This one is still slower than first one though.
+bubbleSortBy'' ::  Predicate a -> [a] -> [a]
+bubbleSortBy'' f as = bubbleSortNBy (length as) f as
 
 -- | Implementation of proper bubble sorting with reducing inner loop and
 -- with no reverses. This version seems to be slower, than above one.
@@ -36,5 +56,6 @@ bubbleSortBy' f xs = case innerSort xs of
                where n = length as
 
 main :: IO ()
-main = mapM_ (perform ((<),(>))) [ bubbleSortBy'
-                                 , bubbleSortBy]
+main = mapM_ (perform ((<),(>))) [ bubbleSortBy''
+                                 , bubbleSortBy'
+                                 , bubbleSortBy ]
