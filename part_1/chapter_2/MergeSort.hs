9f2c11e0db922020fb6fd395ec0ed50b355470a6
sukhmel <sukhmel.v@gmail.com>
2014-05-18 23:53:49 +0400

    Changes in inversion count.

--- part_1/chapter_2/MergeSort.hs
+++ part_1/chapter_2/MergeSort.hs
@@ -19,33 +19,40 @@ module MergeSort where
 import SortingTests
 import Control.Monad.Writer
 
-countInversions ::  MonadWriter [t] m => t -> m ()
-countInversions i = writer ((), [i])
+inversions ::  MonadWriter [(t, t)] m => t -> [t] -> m ()
+inversions i js = writer ((), [(i, j) | j <- js])
 
-mergeBy :: MonadWriter [Int] m
+mergeBy :: MonadWriter [(Int, Int)] m
         => (a -> a -> Bool)
-        -> [a] -> [a] -> m [a]
+        ->   [(a, Int)]
+        ->   [(a, Int)]
+        -> m [(a, Int)]
 mergeBy _ [] b = return b
 mergeBy _ a [] = return a
 mergeBy f (a:as)
-          (b:bs) = if a `f` b
+          (b:bs) = if fst a `f` fst b
                       then do rest <- mergeBy f as (b:bs)
                               return $ a:rest
-                      else do countInversions $ length as + 1
+                      else do inversions (snd b) $ map snd (a:as)
                               rest <- mergeBy f (a:as) bs
                               return $ b:rest
 
 mergeSortBy       :: (a -> a -> Bool) -> [a] -> [a]
-mergeSortBy f = fst . runWriter . performMergeSortBy f
+mergeSortBy f = map fst . fst . runWriter . mergeSortWrapper f
 
 -- | Task 2.4 (e) count all invesions in a list pairs (i, j) where i < j, but
 -- a[j] < a[i]
 countInverses :: Ord a => [a] -> Int
-countInverses = sum . snd . runWriter . performMergeSortBy (<)
+countInverses = length . snd . runWriter . mergeSortWrapper (<)
 
-performMergeSortBy :: MonadWriter [Int] m
+
+
+mergeSortWrapper f xs = performMergeSortBy f $ zip xs [0..]
+
+performMergeSortBy :: MonadWriter [(Int, Int)] m
                    => (a -> a -> Bool)
-                   -> [a] -> m [a]
+                   ->   [(a, Int)]
+                   -> m [(a, Int)]
 performMergeSortBy _ [ ]    = return [ ]
 performMergeSortBy _ [a]    = return [a]
 performMergeSortBy f xs     = do
@@ -53,6 +60,8 @@ performMergeSortBy f xs     = do
               second <- performMergeSortBy f end
               mergeBy f first second
     where (beg, end) = splitAt (length xs `div` 2) xs
+
+
 
 -- | perform tests with merge sort using quickcheck
 main :: IO ()
